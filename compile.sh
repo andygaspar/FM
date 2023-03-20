df6af7bb67da389870a3719bae1a137d9e4d43b7
andgaspar <andrea.gasparin.87@gmail.com>
2023-03-20 13:37:26 +0100

    sistemato compile

--- compile.sh
+++ compile.sh
@@ -1,5 +1,7 @@
 rm fast_me_
 
+cd src
+
 g++ -w -c test_main.cc fastme.c initialiser.c distance.c bme.c bNNI.c gme.c graph.c heap.c inputs.c interface_options.c interface_utilities.c newick.c NNI.c p_utils.c SPR.c traverse.c random.c  utils.c -fopenmp
 g++ -w -o fast_me_  distance.o bme.o bNNI.o gme.o graph.o heap.o inputs.o interface_options.o interface_utilities.o newick.o NNI.o p_utils.o SPR.o traverse.o utils.o random.o initialiser.o fastme.o test_main.o -lm -fopenmp -lpthread
 
